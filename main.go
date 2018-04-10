2949d6b6b6b7fc29a6a63b290aef08a65d40d8ca
shiyongbin <shiyongbin@ezbuy.com>
2018-04-10 18:09:14 +0800

    note

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/everfore/exc"
+	"github.com/toukii/bytes"
+	"github.com/toukii/goutils"
+)
+
+func init() {
+	dic = make(map[string]*Note)
+	err := toml.Unmarshal(goutils.ReadFile(notesFile), &dic)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// refresh()
+
+}
+
+type Note struct {
+	Val   string
+	Exced bool
+}
+
+var (
+	dic map[string]*Note
+
+	notesFile = "notes.toml"
+)
+
+func refresh() {
+	wr := bytes.NewWriter(make([]byte, 0, 1024))
+	err := toml.NewEncoder(wr).Encode(dic)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(goutils.ToString(wr.Bytes()))
+
+	goutils.WriteFile(notesFile, wr.Bytes())
+}
+
+func main() {
+	args := os.Args
+	size := len(args)
+	switch size {
+	case 0:
+	case 1:
+		fmt.Println("nil args")
+	case 2:
+		GetNote(args[1])
+	default:
+		SetNote(args[1:])
+	}
+}
+
+func SetNote(args []string) {
+	exced := false
+	if args[1] == "-e" {
+		exced = true
+	}
+	val := ""
+	if exced {
+		val = strings.Join(args[2:], " ")
+	} else {
+		val = strings.Join(args[1:], " ")
+	}
+	dic[args[0]] = &Note{
+		Val:   val,
+		Exced: exced,
+	}
+	refresh()
+}
+
+func GetNote(key string) {
+	note, ex := dic[key]
+	if !ex {
+		fmt.Println("note nil")
+		return
+	}
+	if !note.Exced {
+		fmt.Printf("%s\n", note.Val)
+		return
+	}
+	exc.Bash(note.Val).Debug(true).Execute()
+
+	// exc.Bash(fmt.Sprintf("echo '%s'| pbcopy", note.Val)).Debug(false).Execute()
+	// bs, err := exc.Bash("echo `pbpaste`").Debug(false).DoNoTime()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("%s", bs)
+}
